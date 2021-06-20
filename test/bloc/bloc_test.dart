30fc597c0f84d9203fc3adc6b30c9344eb9b9911
Elvis Teles <elvists@gmail.com>
2021-06-19 21:52:36 -0400

    adicionando grupos nos testes

--- test/bloc/bloc_test.dart
+++ test/bloc/bloc_test.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
 import 'superhero_bloc_test.dart' as superhero_bloc_test;
 import 'superheroes_list_bloc_test.dart' as superheroes_list_bloc_test;
 
 void main() {
-  superheroes_list_bloc_test.main();
-  superhero_bloc_test.main();
+  group("bloc test => ", () {
+    superheroes_list_bloc_test.main();
+    superhero_bloc_test.main();
+  });
 }
--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
 import 'home_test.dart' as home_teste;
 import 'detail_test.dart' as detail_test;
 
 void main() {
-  home_teste.main();
-  detail_test.main();
+  group("widget test => ", () {
+    home_teste.main();
+    detail_test.main();
+  });
 }
