99d333df402c44b1deac0f7f507c7c1c15619aca
elvists <elvists@gmail.com>
2021-09-20 09:16:03 -0400

    Update flow_test.dart

--- test/flow/flow_test.dart
+++ test/flow/flow_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'home_screen_test.dart' as home_screen_test;
+//import 'home_screen_test.dart' as home_screen_test;
 import 'detail_screen_test.dart' as detail_screen_test;
 
 void main() {
